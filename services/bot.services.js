6d2a587ebd1fec481e09f5c857c3fc6571c72afe
npsapien3254 <robertdelnilo@gmail.com>
2026-04-25 22:57:17 -0400

    Bot mejorado con estados

--- services/bot.services.js
+++ services/bot.services.js
@@ -1,13 +1,61 @@
-import 'dotenv/config';
-import TelegramBot from 'node-telegram-bot-api';
+import "dotenv/config";
+import TelegramBot from "node-telegram-bot-api";
+import { MESSAGES } from "../informacion/mensajes.js";
 
-const cloud_bot = new TelegramBot(process.env.TOKEN_TELEGRAM_BOT, {
-    polling:true
+import { delKey, getKey, setKey } from "../functions/redis.js";
+
+const cloud_bot = new TelegramBot(
+  process.env.TOKEN_TELEGRAM_BOT ||
+    "8335474400:AAEQzO3-lkt0SI5qb00wsiDIhuE3sjW1GSo",
+  {
+    polling: true,
+  },
+);
+
+cloud_bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  let message = await getKey(chatId, "last-message");
+  const step1 = await getKey(chatId, "step1");
+  const step2 = await getKey(chatId, "step2");
+  const step3 = await getKey(chatId, "step3");
+
+  if (!step1) {
+    await setKey(chatId, "step1", "success");
+    await setKey(chatId, "last-message", MESSAGES.text);
+    message = await getKey(chatId, "last-message");
+    return cloud_bot.sendMessage(chatId, message);
+  }
+
+  if (!step2 && !isNaN(parseInt(text))) {
+    await setKey(chatId, "step2", text);
+    await setKey(
+      chatId,
+      "last-message",
+      MESSAGES["opciones"][parseInt(text) - 1]["info"]["text"],
+    );
+    message = await getKey(chatId, "last-message");
+    return cloud_bot.sendMessage(chatId, message);
+  }
+
+  if (!step3 && !isNaN(parseInt(text))) {
+    await setKey(
+      chatId,
+      "last-message",
+      MESSAGES["opciones"][parseInt(step2) - 1]["info"]["opciones"][
+        parseInt(text) - 1
+      ]["text"],
+    );
+    message = await getKey(chatId, "last-message");
+
+    if (
+      !MESSAGES["opciones"][parseInt(step2) - 1]["info"]["opciones"][
+        parseInt(text) - 1
+      ]["opciones"]
+    ) {
+      await delKey(chatId);
+    }
+    return cloud_bot.sendMessage(chatId, message);
+  }
 });
-
-cloud_bot.on("message", (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-
-    cloud_bot.sendMessage(chatId, text);
-})
